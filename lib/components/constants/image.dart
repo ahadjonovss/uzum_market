b6dce0d8ec667c41bcd26f5498a0443ec74e5828
Samandar <samandarahadjonov@gmail.com>
2022-11-27 18:16:45 +0500

    splash page done

---
+++ lib/components/constants/image.dart
@@ -0,0 +1,12 @@
+class AppImages{
+  static const pv_1="assets/images/pv_1.jpg";
+  static const pv_2="assets/images/pv_2.jpg";
+  static const pv_3="assets/images/pv_3.jpg";
+  static const pv_4="assets/images/pv_4.jpg";
+
+  //app logo
+  static const logo="assets/images/logo.png";
+
+
+
+}
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:uzum_market/components/constants/image.dart';
+import 'package:uzum_market/pages/home/tab/tabs/main_page.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Future.delayed(Duration(seconds: 3)).then((value) =>
+        Navigator.pushAndRemoveUntil(context,
+            MaterialPageRoute(builder: (_) => MainPage()), (route) => false));
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          AppImages.logo,
+          width: 120,
+        ),
+      ),
+    );
   }
 }
