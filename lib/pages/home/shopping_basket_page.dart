34d394a1be028252f1cd154777f1d6e5cad447ea
Samandar <samandarahadjonov@gmail.com>
2022-11-27 18:08:20 +0500

    Project files created

---
+++ lib/pages/home/shopping_basket_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ShoppingBasketPage extends StatefulWidget {
+  const ShoppingBasketPage({Key? key}) : super(key: key);
+
+  @override
+  State<ShoppingBasketPage> createState() => _ShoppingBasketPageState();
+}
+
+class _ShoppingBasketPageState extends State<ShoppingBasketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
